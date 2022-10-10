70a2d08caf51d16e2a17c0a5780ed60ca8c7337f
aravindsabu <aravindsabu2001@gmail.com>
2022-10-10 14:28:52 +0530

    to

---
+++ src/app/addnavbar/addnavbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addnavbar',
+  templateUrl: './addnavbar.component.html',
+  styleUrls: ['./addnavbar.component.css']
+})
+export class AddnavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
